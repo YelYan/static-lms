1fc0853a2a5aad1eff144a6020d9e4af08f751d9
YelYan <yelyan.researchandproduction@gmail.com>
2025-08-27 00:11:00 +0630

    [add] shadcn setup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-center text-4xl text-red-500">Hello world</h1>
+
+      <Button
+        variant={"default"}
+        className="text-green-500 cursor-pointer bg-yellow-400"
+      >
+        Click me
+      </Button>
     </div>
   );
 };
